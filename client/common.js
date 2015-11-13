b26f18f24bea6686026c91dbcdd82c675371a182
jayanth <jayanth.bagare@gmail.com>
2015-11-13 13:12:53 +0530

    Before React branching

--- client/common.js
+++ client/common.js
@@ -13,6 +13,9 @@ Template.home.onRendered(function(){
   });
 });
 
+//Set the Autoform Template.
+//AutoForm.setDefaultTemplate('materialize'); 
+
 Meteor.startup(function(){
   loadFilePicker('AnPk1pu8QKe93T4BNlqlxz');
 });
